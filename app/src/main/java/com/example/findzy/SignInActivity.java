07cc74abd820bf4fc13f0d0be44e1aa9fe15f89b
Thaweesha Senanayake <thaweeshasenanayake@gmail.com>
2022-10-10 13:06:56 +0530

    Findzy-Mobile-App

--- app/src/main/java/com/example/findzy/SignInActivity.java
+++ app/src/main/java/com/example/findzy/SignInActivity.java
@@ -2,25 +2,47 @@ package com.example.findzy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SignInActivity extends AppCompatActivity {
-    TextView btnSignUpforAccount;
-    BottomNavigationView bottomNavigationView;
+
+    private TextView btnSignUpforAccount;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_activity);
 
-        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        // action bar color change to white
+        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.color.white));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setElevation(0);
 
+        // add image to the action bar
+//        actionBar.setDisplayShowCustomEnabled(true);
+//        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View v= inflater.inflate(R.layout.custom_img, null);
+//        actionBar.setCustomView(v);
+
+
+
+
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
         btnSignUpforAccount = findViewById(R.id.btn_signup_for_account);
+
+
 
         btnSignUpforAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,12 +51,12 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
 
     @Override
     public void onBackPressed() {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
-
     }
 }
