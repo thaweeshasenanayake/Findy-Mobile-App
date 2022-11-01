cdaf963ec6f63932a91582b64ac1369d16e7db72
Thaweesha Senanayake <thaweeshasenanayake@gmail.com>
2022-11-01 12:20:53 +0530

    Findzy-Mobile-App

--- app/src/main/java/com/example/findzy/AddNewPost.java
+++ app/src/main/java/com/example/findzy/AddNewPost.java
@@ -25,19 +25,65 @@ public class AddNewPost extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
-
-
-        // Spinner dropdown = findViewById(R.id.input_service_category);
-
-        String[] items = new String[]{"Home Maintenance", "Electronic Repairs", "Vehicle Repairs", "Education", "State Service", "IT Services"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+        // service category dropdown
+        String[] itemsOfServiceCategory = new String[]{"Home Maintenance", "Electronic Repairs", "Vehicle Repairs", "Education", "State Service", "IT Services"};
+        ArrayAdapter<String> adapterOfCategory = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
-                items);
+                itemsOfServiceCategory);
 
         AutoCompleteTextView service_category_dropdown = findViewById(R.id.input_service_category);
-        service_category_dropdown.setAdapter(adapter);
+        service_category_dropdown.setAdapter(adapterOfCategory);
         service_category_dropdown.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // do in what to do with dropdown
+            }
+        });
+
+        // service location province dropdown
+
+        String[] itemsOfProvince = new String[]{"Southern", "Western", "Northern", "Uva", "Central"};
+        ArrayAdapter<String> adapterOfProvince = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                itemsOfProvince);
+
+        AutoCompleteTextView service_location_province = findViewById(R.id.input_service_province);
+        service_location_province.setAdapter(adapterOfProvince);
+        service_location_province.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // do in what to do with dropdown
+            }
+        });
+
+        // service location district dropdown
+        String[] itemsOfDistrict = new String[]{"Southern", "Western", "Northern", "Uva", "Central"};
+        ArrayAdapter<String> adapterOfDistrict = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                itemsOfDistrict);
+
+        AutoCompleteTextView service_location_district = findViewById(R.id.input_service_district);
+        service_location_district.setAdapter(adapterOfDistrict);
+        service_location_district.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // do in what to do with dropdown
+            }
+        });
+
+        // service location city dropdown
+        String[] itemsOfCity = new String[]{"Southern", "Western", "Northern", "Uva", "Central"};
+        ArrayAdapter<String> adapterOfCity = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                itemsOfCity);
+
+        AutoCompleteTextView service_location_city = findViewById(R.id.input_service_city);
+        service_location_city.setAdapter(adapterOfCity);
+        service_location_city.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // do in what to do with dropdown
