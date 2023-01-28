4fe56f4da045e2af6ec4d8f3f694b53733d2a342
Thaweesha Senanayake <thaweeshasenanayake@gmail.com>
2023-01-28 13:46:58 +0530

    Findzy-Mobile-App

--- app/src/main/java/com/example/findzy/Profile.java
+++ app/src/main/java/com/example/findzy/Profile.java
@@ -1,20 +1,48 @@
 package com.example.findzy;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Profile extends AppCompatActivity {
 
     Button btnSignOut;
+    Button changeProfilePicture;
+    ShapeableImageView profilePicture;
+    StorageReference storageReference;
+    StorageReference getProfilePicReference;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    ProgressDialog progressDialog;
+    String userID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +57,40 @@ public class Profile extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         btnSignOut = findViewById(R.id.btn_sign_out);
+        profilePicture = findViewById(R.id.profile_picture);
+        changeProfilePicture = findViewById(R.id.btn_change_profile_picture);
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userID = user.getUid();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        getProfilePicReference = FirebaseStorage.getInstance().getReference().child("Users/"+userID);
+
+
+        try {
+            File localFile  = File.createTempFile(userID, "jpg");
+            getProfilePicReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Uri imageUri = Uri.fromFile(localFile);
+                            profilePicture.setImageURI(imageUri);
+                            progressDialog.dismiss();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            profilePicture.setImageDrawable(getDrawable(R.drawable.ic_baseline_person_24));
+                            progressDialog.dismiss();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +109,48 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+        changeProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery,1000);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                profilePicture.setImageURI(imageUri);
+                
+                uploadImageToFirebase(imageUri);
+            }
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading Profile Picture");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        StorageReference fileRef = storageReference.child("Users").child(userID);
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                progressDialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
 
     }
 }
